bf5af4690b704465d04842170fb64187fe962340
idkitsspicy <lalasa0613@gmail.com>
2025-03-26 20:48:33 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -21,8 +21,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # ✅ Load Google Cloud Credentials from GitHub Secrets
-google_credentials = json.loads(os.getenv("google_cred"))
-
+google_credentials = json.loads(os.getenv("GOOGLE_CRED"))
 # ✅ Save Google credentials as a temporary JSON file
 with open("service-account-key.json", "w") as f:
     json.dump(google_credentials, f)
